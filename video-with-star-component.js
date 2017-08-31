17239a96c569653016b5a10d2cd9ee59111f18d9
Anwesh43 <anweshthecool0@gmail.com>
2017-08-31 18:39:10 +0530

    creating a continuous stream of images from the webcam

--- video-with-star-component.js
+++ video-with-star-component.js
@@ -11,12 +11,28 @@ class VideoWithStarComponent extends HTMLElement {
           canvas.width = w
           canvas.height = h
           const context = canvas.getContext('2d')
+          context.globalAlpha = 1
+          context.drawImage(this.video,0,0,w,h)
+          context.globalAlpha = 0.5
           context.fillStyle = 'red'
           context.fillRect(0,0,w,h)
           this.img.src = canvas.toDataURL()
       }
       connectedCallback() {
-          this.render()
+          this.createVideo()
+      }
+      createVideo() {
+          this.video = document.createElement('video')
+          this.video.autoplay = true
+          this.video.width = w
+          this.video.height = h
+          window.navigator.getUserMedia = window.navigator.getUserMedia || window.navigator.webkitGetUserMedia || window.navigator.mozGetUserMedia || window.navigator.oGetUserMedia
+          window.navigator.getUserMedia({audio:false,video:true},(stream)=>{
+              this.video.src = window.URL.createObjectURL(stream)
+              this.render()
+          },(err)=>{
+              alert(err)
+          })
       }
 }
 customElements.define('video-with-stars-comp',VideoWithStarComponent)
