bed0277271641cbbf78a54c323541acd930765f9
Anwesh43 <anweshthecool0@gmail.com>
2017-08-31 18:19:25 +0530

    defining a component and using it

--- video-with-star-component.js
+++ video-with-star-component.js
@@ -1,8 +1,22 @@
+const w = window.innerWidth,h = window.innerHeight
 class VideoWithStarComponent extends HTMLElement {
       constructor() {
           super()
+          const shadow = this.attachShadow({mode:'open'})
+          this.img = document.createElement('img')
+          shadow.appendChild(this.img)
+      }
+      render() {
+          const canvas = document.createElement('canvas')
+          canvas.width = w
+          canvas.height = h
+          const context = canvas.getContext('2d')
+          context.fillStyle = 'red'
+          context.fillRect(0,0,w,h)
+          this.img.src = canvas.toDataURL()
       }
       connectedCallback() {
-
+          this.render()
       }
 }
+customElements.define('video-with-stars-comp',VideoWithStarComponent)
